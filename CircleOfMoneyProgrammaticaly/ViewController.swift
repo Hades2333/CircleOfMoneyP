fc91e35dc79e79d084c5be651b90efe63ee81023
GreatHellizar23th <greenavalonbarrel23@gmail.com>
2021-02-19 13:04:19 +0300

    initial commit

---
+++ CircleOfMoneyProgrammaticaly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CircleOfMoneyProgrammaticaly
+//
+//  Created by Hellizar on 19.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
