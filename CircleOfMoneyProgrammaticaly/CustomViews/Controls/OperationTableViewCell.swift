fce0d0587a1cb0fdff664a5b43a12ba84f453f9d
GreatHellizar23th <greenavalonbarrel23@gmail.com>
2021-03-02 20:27:40 +0300

    ADDED OperationsViewController logic

---
+++ CircleOfMoneyProgrammaticaly/CustomViews/Controls/OperationTableViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  OperationTableViewCell.swift
+//  CircleOfMoneyProgrammaticaly
+//
+//  Created by Hellizar on 2.03.21.
+//
+
+import UIKit
+
+class OperationTableViewCell: UITableViewCell {
+
+    //MARK: - IBOutlets
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    @IBOutlet weak var accountImage: UIImageView!
+    @IBOutlet weak var accountLabel: UILabel!
+
+    @IBOutlet weak var moneyLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+
+    //MARK: - Preperties for easy registration
+    static let identifier =  "OperationTableViewCell"
+
+    //MARK: - Methods for easy registration
+    static func nib() -> UINib {
+        return UINib(nibName: "OperationTableViewCell", bundle: nil)
+    }
+
+    public func configureOperations(_ account: Operations) {
+        self.categoryImage.image = account.category.image.getImage(with: account.category.color.uiColor)!
+        self.categoryLabel.text = account.category.nameOfAccount
+
+        self.accountImage.image = account.account.image.getImage()
+        self.accountLabel.text = account.account.nameOfAccount
+
+        self.moneyLabel.text = "\(account.amountOfMoney) \(account.currency)"
+        self.dateLabel.text = String("\(account.date)")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
+
--- CircleOfMoneyProgrammaticaly/MVC/TabBar/Controller/OperationsViewController.swift
+++ CircleOfMoneyProgrammaticaly/MVC/TabBar/Controller/OperationsViewController.swift
@@ -9,13 +9,75 @@ import UIKit
 
 class OperationsViewController: UIViewController {
 
+    //MARK: - GUI Variables
+    private lazy var operationTable: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = UIColor(named: "mainBackgroundColor")
+        table.register(OperationTableViewCell.nib(),
+                       forCellReuseIdentifier:  OperationTableViewCell.identifier)
+        return table
+    }()
+
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = UIColor(named: "mainBackgroundColor")
-        self.edgesForExtendedLayout = []
+        tuneViews()
+        NotificationCenter.default.addObserver(self ,
+                                               selector: #selector(updateOperations),
+                                               name: .operationsUpdate,
+                                               object: nil)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(Notification.Name.operationsUpdate)
+    }
+
+    //MARK: - Methods
+    func tuneViews() {
+        self.operationTable.dataSource = self
+        self.operationTable.delegate = self
+
+        self.edgesForExtendedLayout = []
+        view.addSubview(self.operationTable)
+
+        self.operationTable.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+    }
+
+    @objc func updateOperations() {
+        self.operationTable.reloadData()
+    }
+}
+
+//MARK: - UITableViewDelegate
+extension OperationsViewController: UITableViewDelegate {
+    
+}
+
+//MARK: - OperationsViewController
+extension OperationsViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        myOperations.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: OperationTableViewCell.identifier,
+                for: indexPath ) as? OperationTableViewCell else {
+            fatalError("Misconfigured cell type for cell!")
+        }
+        cell.configureOperations(myOperations[indexPath.row])
+        return cell
+    }
 
 }
